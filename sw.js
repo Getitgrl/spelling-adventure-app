98cbfdd2d8830c975520f625551efeef95c66a3a
Getitgrl <nsyork112@yahoo.com>
2026-08-21 06:57:23 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(Promise.all([
+    self.registration.unregister(),
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  ]));
+});
