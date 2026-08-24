baed37eb23d3ae811c4735391328e9f34eb2433d
Getitgrl <nsyork112@yahoo.com>
2026-08-24 18:52:34 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", event => self.skipWaiting());
-self.addEventListener("activate", event => {
-  event.waitUntil(Promise.all([
-    self.registration.unregister(),
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-  ]));
-});
